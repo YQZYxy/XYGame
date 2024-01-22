dfc3cf112170d71c9ce4dbe59bcc04d39406163a
YQZYxy <2487334868@qq.com>
2024-01-22 22:00:11 +0800

    1

---
+++ Source/YQZY/Game/RoleModule/RoleModule.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/YQZYCharacter.h"
+#include "RoleModule.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YQZY_API ARoleModule : public AYQZYCharacter
+{
+	GENERATED_BODY()
+	
+};
