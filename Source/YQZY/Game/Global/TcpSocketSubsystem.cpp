0a92fb30ebd6888c80c84ce81609763022a179e3
YQZYxy <83347062+YQZYxy@users.noreply.github.com>
2024-03-06 17:36:55 +0800

    1

--- Source/YQZY/Game/Global/TcpSocketSubsystem.cpp
+++ Source/YQZY/Game/Global/TcpSocketSubsystem.cpp
@@ -22,19 +22,61 @@ void UTcpSocketSubsystem::Deinitialize()
 	Super::Deinitialize();
 }
 
+void UTcpSocketSubsystem::Shutdown()
+{
+    if (m_NativeSocket->IsConnected()) {
+		m_NativeSocket->Close();
+	} 
+	Super::Shutdown();
+}
+
 void UTcpSocketSubsystem::OnTest()
 {
     P_RoleData root;
 
-    m_NativeSocket = FWebSocketsModule::Get().CreateWebSocket(m_Url);
+    m_NativeSocket = FWebSocketsModule::Get().CreateWebSocket("ws://192.168.3.101:3723/webSocketServer/ue");
 
-    m_NativeSocket->OnConnected().AddUObject(this, &UInWebSocketSession::OnConnected);
-    m_NativeSocket->OnClosed().AddUObject(this, &UInWebSocketSession::OnClosed);
-    m_NativeSocket->OnMessage().AddUObject(this, &UInWebSocketSession::OnMessage);
-    m_NativeSocket->OnRawMessage().AddUObject(this, &UInWebSocketSession::OnRawMessage);
-    m_NativeSocket->OnConnectionError().AddUObject(this, &UInWebSocketSession::OnError);
+    m_NativeSocket->OnConnected().AddUObject(this, &UTcpSocketSubsystem::OnConnected);
+    m_NativeSocket->OnConnectionError().AddUObject(this, &UTcpSocketSubsystem::OnConnectionError);
+    m_NativeSocket->OnClosed().AddUObject(this, &UTcpSocketSubsystem::OnClosed);
+    m_NativeSocket->OnMessage().AddUObject(this, &UTcpSocketSubsystem::OnMessage);
+    m_NativeSocket->OnMessageSent().AddUObject(this, &UTcpSocketSubsystem::OnMessageSent);
+
+    m_NativeSocket->OnRawMessage().AddUObject(this, &UTcpSocketSubsystem::OnRawMessage);
 
     m_NativeSocket->Connect();
     m_NativeSocket->Send(Message.GetData(), sizeof(uint8) * Message.Num(), bIsBinary);
     m_NativeSocket->Send(Message);
 }
+
+void UUWebSocketGameInstance::OnConnected()
+{
+
+}
+
+void UUWebSocketGameInstance::OnConnectionError(const FString& Error)
+{
+
+}
+
+void UUWebSocketGameInstance::OnClosed(int32 StatusCode, const FString& Reason, bool bWasClean)
+{
+
+}
+void UUWebSocketGameInstance::OnMessage(const FString& Message)
+{
+
+}
+
+void UUWebSocketGameInstance::OnMessageSent(const FString& MessageString)
+{
+    
+}
+
+void UUWebSocketGameInstance::OnRawMessage()
+{
+
+}
+
+
+ 
