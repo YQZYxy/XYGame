765cc86f5e69ebc2f9ea1440e12aadc0d591cd83
YQZYxy <83347062+YQZYxy@users.noreply.github.com>
2024-03-16 01:02:40 +0800

    1

--- Source/YQZY/Game/Global/TcpSocketSubsystem.cpp
+++ Source/YQZY/Game/Global/TcpSocketSubsystem.cpp
@@ -34,21 +34,21 @@ void UTcpSocketSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 
 	this->ConnectToServer("192.168.3.102", 3723);
 
-	Role_Data RoleData;
+	//Role_Data RoleData;
 
-	RoleData.set_role_id(9527);
-	RoleData.set_role_name("桂林仔");
+	//RoleData.set_role_id(9527);
+	//RoleData.set_role_name("桂林仔");
 
-	int32 msg_type = 100;
+	//int32 msg_type = 100;
 
-	TArray<uint8> Msg;
-	Msg.SetNumUninitialized(FMath::Min(RoleData.ByteSize() + 4, 65535));
+	//TArray<uint8> Msg;
+	//Msg.SetNumUninitialized(FMath::Min(RoleData.ByteSize() + 4, 65535));
 
-	memset(Msg.GetData(), 0, Msg.Num());
-	memcpy(Msg.GetData(), &msg_type, 4);
-	RoleData.SerializeToArray(Msg.GetData() + 4, Msg.Num() - 4);
+	//memset(Msg.GetData(), 0, Msg.Num());
+	//memcpy(Msg.GetData(), &msg_type, 4);
+	//RoleData.SerializeToArray(Msg.GetData() + 4, Msg.Num() - 4);
 
-	this->SendToServer(Msg);
+	//this->SendToServer(Msg);
 }
  
 void UTcpSocketSubsystem::Deinitialize()
